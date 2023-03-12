08056bbba8a49873533aebab347182d43f5d6ab4
Sandro Fernandes <sandrofernandesrosal@gail.com>
2023-03-12 17:40:12 -0300

    refatorando

--- src/Pages/projetos/DataProjeto.jsx
+++ src/Pages/projetos/DataProjeto.jsx
@@ -5,7 +5,7 @@ export const DataProjetos = [
 
   {img: "../img/portfolio-novo.png",
   saiba: "Meu Portfólio atual",
-  tecnologia: "Projeto feito em ReactJS. Fiz esse projeto com a intenção de fazer um portfólio melhor do que o antigo, e para consolidar o meu aprendizado em ReactJS.",
+  tecnologia: "Projeto feito em ReactJS. Espaço onde coloco meus projetos.",
   acessar: "https://sandrofernandesdev.netlify.app/",
   repositorio: "https://github.com/SandroFernandesRosal/Portf-lio-ReactJS"
   
@@ -13,7 +13,7 @@ export const DataProjetos = [
 
   {img: "../img/rpz-novo.png",
    saiba: "RPZ FC - Time de futebol amador ",
-   tecnologia: "Projeto feito em ReactJS. Refiz do zero um projeto antigo, onde pude por em prática o aprendizado em ReactJS. ",
+   tecnologia: "Projeto feito em ReactJS. Refiz do zero um projeto antigo. ",
    acessar: "https://rpzfc.netlify.app/",
    repositorio: "https://github.com/SandroFernandesRosal/RPZFC-ReactJS"
    
@@ -21,7 +21,7 @@ export const DataProjetos = [
 
   {img: "../img/portfolio-antigo.png",
   saiba: "Meu primeiro potfólio",
-  tecnologia: "Projeto feito com HTML e CSS. Esse foi meu primeiro portfólio, feito em 2020 e atualizado até início de 2022",
+  tecnologia: "Projeto feito com HTML e CSS. Esse foi meu primeiro portfólio, feito em 2020 e atualizado até o início de 2022.",
   acessar: "https://sandrofernandesrosal.github.io/Sandro-Fernandes",
   repositorio: "https://github.com/SandroFernandesRosal/Sandro-Fernandes"
   
